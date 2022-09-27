7f9b3bf82c2910c3bf49d17d453b0bb619818d87
Dean Huang <hcd@mojopot.com>
2022-09-27 14:55:17 +0800

    edit text

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,9 @@
         :current="pagesParams.page"
         #="{ totalPages, totalItems, nextPage, prevPage, current }"
       >
-        <h2 class="text-3xl font-bold mt-10 mb-3">Pagination with renderless component (hide when total pages {{`<=`}} 1)</h2>
+        <h2 class="text-3xl font-bold mt-10 mb-3">
+          Pagination with renderless component (hide when total pages {{`<=`}} 1)
+        </h2>
         <div v-if="totalPages > 1" class="flex justify-center justify-between">
           <div class="flex gap-3 items-center">
             <label>每頁顯示</label>
